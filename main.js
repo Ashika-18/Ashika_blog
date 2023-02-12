0beec25a27b67f3c3a63a58ba4b6e5d4f37e18ba
Ashika <117647146+Ashika-18@users.noreply.github.com>
2023-02-12 16:58:56 +0900

    改良

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-const picArray = [
-    {src: './images/朝日.jpg'},
-    {src: './images/昼.jpg'},
-    {src: './images/夕焼け.jpg'},
-    {src: './images/ポセイドン.jpg'}
+var picArray = [
+    { src: './images/朝日.jpg' },
+    { src: './images/昼.jpg' },
+    { src: './images/夕焼け.jpg' },
+    { src: './images/ポセイドン.jpg' }
 ];
-
-let counter = 0;
-
-const changePic = () => {
+var counter = 0;
+var changePic = function () {
     if (counter < picArray.length) {
         document.getElementById('pics').src = picArray[counter].src;
-        counter++
-    } else {
+        counter++;
+    }
+    else {
         document.getElementById('pics').src = picArray[0].src;
         counter = 1;
     }
 }
 
-let playId = 0;
-
-const slideShow = () => {
-    if (playId === 0) {
+var playId = 0;
+var slideShow = function () {
+    if (playId == 0) {
         document.getElementById('play').innerHTML = 'STOP';
         playId = setInterval(changePic, 2000);
-    } else {
+    }
+    else {
         document.getElementById('play').innerHTML = 'START';
         clearInterval(playId);
         playId = 0;
     }
 }
+
+//alert('JavaScriptは読みこんでるよ!');
